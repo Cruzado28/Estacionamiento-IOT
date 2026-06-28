849e578d7d42a9d308fbfec8dc3a6e5fad8c7c84
Cruzado28 <jcruzadoz2@upao.edu.pe>
2026-06-27 22:27:57 -0500

    Corregir zona horaria de MySQL en Railway

--- config/db.js
+++ config/db.js
@@ -7,6 +7,9 @@ const pool = mysql.createPool({
   password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
   database: process.env.MYSQLDATABASE || process.env.DB_NAME,
   port: Number(process.env.MYSQLPORT || process.env.DB_PORT || 3306),
+
+  timezone: "-05:00",
+  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
